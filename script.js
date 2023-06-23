1ad3e347a96c545a9f2f03d67f03fa88c12d1675
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-23 11:22:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+window.onload = function() {
+  var targetElement = document.getElementById("level");
+  var domLevel = 0;
+
+  while (targetElement.parentElement) {
+    targetElement = targetElement.parentElement;
+    domLevel++;
+  }
+
+  console.log(alert("The level of the element is:"+domLevel));
+};
